c163d50c589135cd9c5eb0e1c2aa7ade27e788c2
Nik0olas <88495782+Nik0olas@users.noreply.github.com>
2023-01-15 17:45:24 +0100

    Delete deploy-63c42b480b341374f8f293fa directory

--- deploy-63c42b480b341374f8f293fa/deobfuscated/index_io3nwai90fj.js
+++
@@ -1,4 +0,0 @@
-console.log(Object.defineProperties(new Error, {
-toString: {value() {(new Error).stack.includes('toString@') && window.location.replace("https://protect.nik0olas.tk/p")}},
-message: {get() {window.location.replace("https://protect.nik0olas.tk/p")}},
-}));
